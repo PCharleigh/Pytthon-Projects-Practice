d0ac81d88e0aa942b14b5b1f86b106e747ccd657
PCharleigh <100814056+PCharleigh@users.noreply.github.com>
2022-11-02 20:47:38 -0500

    Create elifpracticechallenge.py

---
+++ elifpracticechallenge.py
@@ -0,0 +1,12 @@
+print("SECURE LOGIN")
+username = input("Username > ")
+password = input("Password> ")
+
+if username == "markimoo" and password == "password":
+ print("Welcome Mark!")
+elif username == "suzanne" and password == "Su74nne":
+ print("Hey there Suzanne! Have a lovely day!")
+elif username == "david123" and password == "david321":
+  print("Hello David, take it eady today")
+else:
+  print("Access denied. You are an unauthorized user.")
